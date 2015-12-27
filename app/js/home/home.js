a82aeaccfa2a3bf827b61669a676d8cf44f9dabe
Antonio <antoniom13>
2015-12-27 17:28:02 -0500

    Parses and passes messages to template

--- app/js/home/home.js
+++ app/js/home/home.js
@@ -2,16 +2,26 @@
 
 angular.module('WynTech.home', [])
 
-.controller('HomeCtrl', ['$scope', '$document', function($scope,$document){
+.controller('HomeCtrl', ['$scope', '$document', '$firebaseArray', function($scope,$document, $firebaseArray){
   console.log('HomeCtrl Loaded');
 
   var top = 0;
   var duration = 500;
   var offset = 50;
 
+  var generalMessageRef = new Firebase("https://wyntech.firebaseio.com/general");
+
+  generalMessageRef.on("value", function(snapshot){
+    console.log(snapshot.val());
+  }, function (errorObject){
+    console.log(erroObject);
+  });
+
   $scope.scrollEvent = function(id){
     var whatWeDoElement = angular.element(document.getElementById(id));
     $document.scrollToElement(whatWeDoElement, offset, duration)
-  }
+  };
+
+  $scope.generalMessages = $firebaseArray(generalMessageRef);
 
 }]);
