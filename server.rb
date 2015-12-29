e21866aaba466b132ef8e3a416fb343c1804e76f
Antonio <antonio@blubeta.com>
2015-12-29 22:30:43 +0000

    Bind to all addresses for now

--- server.rb
+++ server.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 
 set :public_folder, 'app'
-#set :port, 80
-#set :bind, '0.0.0.0'
+set :port, 8080
+set :bind, '0.0.0.0'
 
 get '/' do
   File.read(File.join('app', 'index.html'))
